6a3e3188fb670bfb4408a8406abd76d5e48f2331
wee-owl <kem-cv@yandex.ru>
2023-05-05 14:49:42 +0300

    fix: update file

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,22 @@
 export default class Character {
-  constructor(name, type, attack, protection) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = attack;
-    this.protection = protection;
-
+  constructor(name, type) {
     const typeArr = ['Bowman', 'Swordsman', 'Magician', 'Undead', 'Zombie', 'Daemon'];
-    const typeInArr = typeArr.some((el) => el === type);
 
     if (typeof (name) !== 'string' || !(name.length >= 2 && name.length <= 10)) {
       throw new Error('Invalid value (string length must be between 2 and 10)');
+    } else {
+      this.name = name;
     }
 
-    if (typeof (type) !== 'string' || typeInArr === false) {
+    if (typeof (type) !== 'string' || !typeArr.includes(type)) {
       throw new Error('Invalid character name value');
+    } else {
+      this.type = type;
     }
+
+    this.health = 100;
+    this.level = 1;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
